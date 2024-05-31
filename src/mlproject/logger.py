e49597a56e70f738034f730623393d252d851bab
Rajesh <rajeshbajaj80@gmail.com>
2024-05-31 13:01:02 +0530

    all commit

--- src/mlproject/logger.py
+++ src/mlproject/logger.py
@@ -1,16 +1,11 @@
-import logging
 import os
+import logging
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_d%_y%_H%_M%_S%')}.log"
 log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(log_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-
+logging.basicConfig(filename=LOG_FILE_PATH,format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",level=logging.INFO)
